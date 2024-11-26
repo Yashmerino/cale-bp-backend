addcc4f5b2e72b9e9a1f4300dcc2f3a9dffa2057
Artiom Bozieac <artiomytprofile@gmail.com>
2024-11-26 17:30:54 +0200

    Initial commit

---
+++ src/main/java/com/mlc/vdsr/VdsrApplication.java
@@ -0,0 +1,16 @@
+package com.mlc.vdsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * VDSR Application class to run the app.
+ */
+@SpringBootApplication
+public class VdsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdsrApplication.class, args);
+	}
+
+}
